0e91c8ca0a3fc33246609be9597ad52f6df843a1
Denomycor <afonsoresteves@hotmail.com>
2022-11-26 17:58:32 +0000

    Added panning over the tileset

---
+++ SewerGodot/assets/ui/upgradeMenu/scripts/UpgradeMenuTiles.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+/* Handles operations of the Upgrade Menu on the tile system
+ * 
+ */
+public class UpgradeMenuTiles : Node2D{
+
+    //Node Vars
+    public TileMap tileMap {get;set;}
+
+    //State vars
+    public bool beingDragged {get;set;}
+
+
+    public override void _Ready() {
+        tileMap = GetNode<TileMap>("TileMap");
+    }
+
+    public override void _UnhandledInput(InputEvent e){
+        if(e.IsActionPressed("pan")){
+            beingDragged = true;
+        }
+        if(e.IsActionReleased("pan")){
+            beingDragged = false;
+        }
+        if(e is InputEventMouseMotion && beingDragged){
+            tileMap.Position += (e as InputEventMouseMotion).Relative;
+        }
+    }
+
+}
