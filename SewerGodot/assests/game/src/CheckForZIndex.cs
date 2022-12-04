d6412b18dc5ce145ebbad0f33cf6f2668561bf06
Joao Vera <joaoguedessoaresvera@gmail.com>
2022-12-04 20:13:56 +0000

    adjusted player and enemy size and fixed some issues with z indexes

--- SewerGodot/assests/game/src/CheckForZIndex.cs
+++ SewerGodot/assests/game/src/CheckForZIndex.cs
@@ -1,8 +1,13 @@
 using Godot;
 using System;
-
+/* Checks is another entity has entered the players space to ajust his Z index according to the heights of the entety and player
+ *
+ */
 public class CheckForZIndex : Area2D
 {
+
+    private bool _intersecting = false;
+    Area2D _other;
 
     //connect signals to self
     public override void _Ready(){
@@ -10,18 +15,32 @@ public class CheckForZIndex : Area2D
         this.Connect("area_shape_exited", this, nameof(_on_Area_area_shape_exited));
     }
 
-    //if collided adjust z index accordingly
-    public void _on_Area_area_shape_entered(RID rid, Area2D other, int index, int local_index){
-        if(((Node2D)other.Owner).Position.y > ((Node2D)this.Owner).Position.y){
-            ((Node2D)this.Owner).ZIndex -=1;
+    public override void _Process(float delta)
+    {
+        if(_intersecting && _other!= null){
+            CheckZIndex(_other);
         }else{
-            ((Node2D)this.Owner).ZIndex +=1;
+            ((Node2D)this.Owner).ZIndex = 0;
         }
     }
 
-    //when exiting collision resent zindex
-    public void _on_Area_area_shape_exited(RID rid, Area2D other, int index, int local_index){
-            ((Node2D)this.Owner).ZIndex = 0;
+    //set intersecting bool to true and set _other to the other entity aread 2d
+    public void _on_Area_area_shape_entered(RID rid, Area2D other, int index, int local_index){
+        _intersecting = true;
+        _other = other;
     }
 
+    //set intersecting bool to false
+    public void _on_Area_area_shape_exited(RID rid, Area2D other, int index, int local_index){
+        _intersecting = false;
+    }
+
+    //if collided adjust z index accordingly
+    private void CheckZIndex(Area2D other){
+        if(((Node2D)other.Owner).Position.y > ((Node2D)this.Owner).Position.y){
+            ((Node2D)this.Owner).ZIndex = -2;
+        }else{
+            ((Node2D)this.Owner).ZIndex = 2;
+        }
+    }
 }
