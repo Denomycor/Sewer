55e68abe1640dbb5ef3682233f45ae5288c59d3c
Denomycor <afonsoresteves@hotmail.com>
2022-12-08 18:23:26 +0000

    First iteration of AutoUpgrade

---
+++ SewerGodot/assets/upgradeSystem/scripts/AutoUpgrade.cs
@@ -0,0 +1,15 @@
+
+/* Autonomous upgrade
+ *
+ */
+public abstract class AutoUpgrade : Upgrade {
+
+    //Constructor
+    public AutoUpgrade(string name, string texture, Type type, Rarity rarity, int valueInt)
+        :base(name, texture, type, rarity, valueInt)
+    {
+    }
+
+    //To be called every frame by player
+    public abstract void Process();
+}
