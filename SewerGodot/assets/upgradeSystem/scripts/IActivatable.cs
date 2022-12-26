3c9895dcc4eb975f9a0840cdc16a2718097dac59
Denomycor <afonsoresteves@hotmail.com>
2022-12-26 18:21:54 +0000

    IActivatable will be created later

---
+++ SewerGodot/assets/upgradeSystem/scripts/IActivatable.cs
@@ -0,0 +1,8 @@
+
+/* Interface for upgrades that utilize user input
+ *
+ */
+public interface IActivatable {
+    //TODO later after it is decided how player abilitiy controls should be implemented
+    
+}
